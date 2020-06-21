592d23a9b34cf9ac44d91f60673902ee049c1347
janusat99 <lasyamatcha1805@gmail.com>
2020-06-21 22:08:11 +0530

    modified linkedlist.py, queues.py with queue implementations

--- linkedlist.py
+++ linkedlist.py
@@ -37,6 +37,51 @@ class linkedList:
                 self._size-=1
                 return ans
 
+    class linkedQueue:
+        '''Queue Implementation through linked lists'''
+        class _Node:
+            def __init__(self, element,after):
+                self._element = element
+                self._after = after
+        def __init__(self):
+            self._head=None
+            self._tail=None
+            self._size=0
+        '''len,is_empty methods'''
+        def is_empty(self):
+            return self._size==0
+        def __len__(self):
+            return self._size
+        
+        def push(self,e):
+            '''pushes e into queue'''
+            node = self._Node(e, None)
+            if(self.is_empty()):
+                self._head=node
+                self._tail=node
+            else:
+                self._tail._after=node
+            self._tail=node
+            self._size+=1
+        def top(self):
+            '''returns first element without removal'''
+            if not(self.is_empty()):
+                return self._head._element
+            else:
+                raise TypeError("Empty Queue")
+        def pop(self):
+            '''returns and removes first element of queue'''
+            if (self.is_empty()):
+                raise TypeError("Empty Queue")
+            else:
+                temp=self._head
+                ans=temp._element
+                self._head=self._head._after
+                self._size-=1
+                if(self.is_empty()):
+                    self._tail=None
+                temp=None
+                return ans
         
 '''
 ll=linkedList.linkedStack()
@@ -48,4 +93,20 @@ print(len(ll))
 ll.push(10)
 print(ll.top())
 print(len(ll))           
+'''
+
+'''check comments for linked list queue implementation'''
+'''
+llq=linkedList.linkedQueue()
+for i in range(5):
+    llq.push(i)
+print(llq.top())
+print(len(llq))
+llq.push(10)
+print(llq.top())
+print(len(llq))
+print(llq.pop())
+print(llq.pop())
+print(llq.pop())
+print(len(llq))
 '''
--- queues.py
+++ queues.py
@@ -20,7 +20,7 @@ class Queue:
             if not(self.is_empty()):
                 return self._data[self._front]
             else:
-                raise Empty("Queue is empty")
+                raise TypeError("Queue is empty")
         def push(self,e):
             '''push element to the possible end of queue'''
             if(self.len()<self.def_cap):
@@ -40,7 +40,7 @@ class Queue:
                     self._front=0
                     self._pos=0
             else:
-                raise Empty("Queue is empty")
+                raise TypeError("Queue is empty")
         def _resize(self):
             self._pos=self.def_cap
             self.def_cap*=2
@@ -52,6 +52,59 @@ class Queue:
             self._front=0
         def print_queue(self):
             print(self._data)
+
+    class linkedQueue:
+        '''Queue Implementation through linked lists'''
+        class _Node:
+            def __init__(self, element, after):
+                self._element = element
+                self._after = after
+
+        def __init__(self):
+            self._head = None
+            self._tail = None
+            self._size = 0
+        '''len,is_empty methods'''
+
+        def is_empty(self):
+            return self._size == 0
+
+        def __len__(self):
+            return self._size
+
+        def push(self, e):
+            '''pushes e into queue'''
+            node = self._Node(e, None)
+            if(self.is_empty()):
+                self._head = node
+                self._tail = node
+            else:
+                self._tail._after = node
+            self._tail = node
+            self._size += 1
+
+        def top(self):
+            '''returns first element without removal'''
+            if not(self.is_empty()):
+                return self._head._element
+            else:
+                raise TypeError("Empty Queue")
+
+        def pop(self):
+            '''returns and removes first element of queue'''
+            if (self.is_empty()):
+                raise TypeError("Empty Queue")
+            else:
+                temp = self._head
+                ans = temp._element
+                self._head = self._head._after
+                self._size -= 1
+                if(self.is_empty()):
+                    self._tail = None
+                temp = None
+                return ans
+
+'''Check comments for Array Queue Implementation'''
 '''
 q=Queue.ArrayQueue()
 print(q.is_empty())
@@ -68,4 +121,19 @@ for i in range(20):
     q.push(100+i)
 print(q.len())
 q.print_queue()
+'''
+'''check comments for linked list queue implementation'''
+'''
+llq=linkedList.linkedQueue()
+for i in range(5):
+    llq.push(i)
+print(llq.top())
+print(len(llq))
+llq.push(10)
+print(llq.top())
+print(len(llq))
+print(llq.pop())
+print(llq.pop())
+print(llq.pop())
+print(len(llq))
 '''
